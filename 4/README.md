dabc820251a394532710411d5bdaf3de493ae156
Antony-Sk <SkorkinAnton@yandex.ru>
2023-07-25 11:54:19 +0300

    Task 4: 15 points

---
+++ 4/README.md
@@ -0,0 +1,8 @@
+15 points now
+
+```text
+ok - too many tasks
+ok - reached max tasks
+        -------- test_thread_pool_max_tasks done --------
+        -------- main done --------
+```
--- 4/thread_pool.c
+++ 4/thread_pool.c
@@ -26,7 +26,8 @@ struct thread_pool {
     size_t count;
     size_t task_cnt;
     pthread_mutex_t mutex;
-
+    pthread_cond_t wait_for_task;
+    int to_delete;
 // task queue
     struct task_list_node *tasks_front;
     struct task_list_node *tasks_back;
@@ -43,6 +44,9 @@ thread_pool_new(int max_thread_count, struct thread_pool **pool) {
     (*pool)->threads = (pthread_t *) malloc(sizeof(pthread_t) * max_thread_count);
     (*pool)->tasks_front = (*pool)->tasks_back = NULL;
     (*pool)->task_cnt = 0;
+    (*pool)->to_delete = 0;
+    pthread_mutex_init(&(*pool)->mutex, NULL);
+    pthread_cond_init(&(*pool)->wait_for_task, NULL);
     return 0;
 }
 
@@ -53,49 +57,65 @@ thread_pool_thread_count(const struct thread_pool *pool) {
 
 int
 thread_pool_delete(struct thread_pool *pool) {
-    for (size_t i = 0; i < pool->count; i++)
-        free(pool->threads[i]);
+    if (__atomic_load_n(&pool->task_cnt, __ATOMIC_ACQUIRE) != 0) {
+        return TPOOL_ERR_HAS_TASKS;
+    }
+    pool->to_delete = 1;
+    pthread_cond_broadcast(&pool->wait_for_task);
+    for (size_t i = 0; i < pool->count; i++) {
+        pthread_join(pool->threads[i], NULL);
+    }
+    pthread_mutex_destroy(&pool->mutex);
+    pthread_cond_destroy(&pool->wait_for_task);
     free(pool->threads);
+    free(pool);
     return 0;
 }
 
-#include <stdio.h>
 void *
 thread_task_worker(void *pool) {
     struct thread_pool *p = pool;
-    pthread_mutex_lock(&p->mutex);
-    while (p->tasks_front != NULL) {
+    while (1) {
+        pthread_mutex_lock(&p->mutex);
+        while (p->tasks_front == NULL) {
+            pthread_cond_wait(&p->wait_for_task, &p->mutex);
+            if (p->to_delete) {
+                pthread_mutex_unlock(&p->mutex);
+                return NULL;
+            }
+        }
         struct thread_task *task = p->tasks_front->task;
-        struct task_list_node *new_front = p->tasks_front->next;
-
-        if (p->tasks_front->prev != NULL)
-            p->tasks_front->prev->next = p->tasks_front->next;
-        if (p->tasks_front->next != NULL)
-            p->tasks_front->next->prev = p->tasks_front->prev;
-        if (new_front == NULL)
+        struct task_list_node *new_fr = p->tasks_front->next;
+        free(p->tasks_front);
+        p->tasks_front = new_fr;
+        if (p->tasks_front == NULL) {
             p->tasks_back = NULL;
-        p->tasks_front = new_front;
-        printf("%p", new_front);
+        } else {
+            p->tasks_front->prev = NULL;
+        }
         pthread_mutex_unlock(&p->mutex);
         __atomic_store_n(&task->state, RUNNING, __ATOMIC_RELAXED);
         task->result = task->function(task->arg);
+        pthread_mutex_lock(&task->mutex);
         __atomic_store_n(&task->state, FINISHED, __ATOMIC_RELAXED);
         pthread_cond_signal(&task->is_finished);
-        pthread_mutex_lock(&p->mutex);
+        __atomic_sub_fetch(&p->task_cnt, 1, __ATOMIC_ACQ_REL);
+        pthread_mutex_unlock(&task->mutex);
     }
-    __atomic_sub_fetch(&p->task_cnt, 1, __ATOMIC_ACQ_REL);
-    pthread_mutex_unlock(&p->mutex);
     return NULL;
 }
 
 int
 thread_pool_push_task(struct thread_pool *pool, struct thread_task *task) {
-    pthread_mutex_lock(&pool->mutex);
+    if (__atomic_load_n(&pool->task_cnt, __ATOMIC_RELAXED) == TPOOL_MAX_TASKS) {
+        return TPOOL_ERR_TOO_MANY_TASKS;
+    }
     struct task_list_node *node = (struct task_list_node *) malloc(sizeof(struct task_list_node));
     task->pool = pool;
     task->state = CREATED;
     node->task = task;
     node->next = NULL;
+    pthread_mutex_lock(&pool->mutex);
     if (pool->tasks_back != NULL) {
         pool->tasks_back->next = node;
         node->prev = pool->tasks_back;
@@ -104,17 +124,17 @@ thread_pool_push_task(struct thread_pool *pool, struct thread_task *task) {
         node->prev = NULL;
         pool->tasks_front = pool->tasks_back = node;
     }
-    __atomic_add_fetch(&pool->task_cnt, 1, __ATOMIC_ACQ_REL);
     pthread_mutex_unlock(&pool->mutex);
+    __atomic_add_fetch(&pool->task_cnt, 1, __ATOMIC_ACQ_REL);
 
     if (pool->count < pool->max_size && pool->task_cnt > pool->count) {
         pthread_t *t = &pool->threads[pool->count];
         if (!pthread_create(t, NULL, thread_task_worker, pool)) {
             pool->count++;
         }
+    } else {
+        pthread_cond_signal(&pool->wait_for_task);
     }
-//    printf("%u\n", pool->tasks_front->task->state);
-//    printf("%u\n", pool->tasks_back->task->state);
     return 0;
 }
 
@@ -143,14 +163,16 @@ thread_task_is_running(const struct thread_task *task) {
 
 int
 thread_task_join(struct thread_task *task, void **result) {
-    pthread_mutex_lock(&task->mutex);
-    if (task->pool == NULL && task->state != FINISHED) {
-        pthread_mutex_unlock(&task->mutex);
+    if (task->pool == NULL) {
         return TPOOL_ERR_TASK_NOT_PUSHED;
     }
+    pthread_mutex_lock(&task->mutex);
+    while (!thread_task_is_finished(task)) {
+        pthread_cond_wait(&task->is_finished, &task->mutex);
+    }
     pthread_mutex_unlock(&task->mutex);
-    pthread_cond_wait(&task->is_finished, &task->mutex);
     *result = task->result;
+    task->pool = NULL;
     return 0;
 }
 
@@ -170,15 +192,14 @@ thread_task_timed_join(struct thread_task *task, double timeout, void **result)
 
 int
 thread_task_delete(struct thread_task *task) {
-    if (task->state == RUNNING)
+    if (thread_task_is_running(task))
         return TPOOL_ERR_TASK_NOT_PUSHED;
     if (task->pool != NULL) {
         return TPOOL_ERR_TASK_IN_POOL;
     }
-    if (task->result != NULL)
-        free(task->result);
     pthread_mutex_destroy(&task->mutex);
     pthread_cond_destroy(&task->is_finished);
+    free(task);
     return 0;
 }
 
